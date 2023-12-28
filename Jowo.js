b0b20ff0dea4933e7c61c6612cf8184d88baceda
HilmiAnarya <hilmidrone@gmail.com>
2023-12-28 13:59:29 +0700

    first commit

---
+++ Jowo.js
@@ -0,0 +1,32 @@
+data = [{
+    'img1' : 'flop with watermelon.jpg',
+    'img2' : 'pringles floppa.jpg', 
+    'title' : 'Web E-Commerce', 
+    'subtitle' : 'Zenit',
+    'desc' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quos, consequuntur accusantium nihil, nulla, quibusdam voluptas aliquam accusamus dolorem est voluptate deserunt. Repellat tempore, fugit eum fuga et quae laudantium!'
+}, {
+    'img1' : 'flop with watermelon.jpg',
+    'img2' : 'pringles floppa.jpg', 
+    'title' : 'Web E-Commerce', 
+    'subtitle' : 'Zenit',
+    'desc' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quos, consequuntur accusantium nihil, nulla, quibusdam voluptas aliquam accusamus dolorem est voluptate deserunt. Repellat tempore, fugit eum fuga et quae laudantium!'
+}]
+
+var container = document.querySelector('.container-project')
+
+data.forEach(element => {
+    container.innerHTML +=
+    `<div class="project">
+    <div class="img-project">
+        <img src="`+element.img1+`" alt="">
+        <img src="${element.img2} " alt="">
+    </div>
+    <div class="desc-project">
+        <h1>${element.title}</h1>
+        <h2>${element.subtitle}</h2>
+        <h2>${element.desc}</h2>
+        <button class="read-more">Read More</button>
+    </div>
+</div>`
+
+});
